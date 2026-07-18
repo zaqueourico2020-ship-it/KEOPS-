a2cb01adbe2a7b518db9fa4a11c8d5a4838f039d
zaqueourico2020-ship-it <zaqueourico2020@gmail.com>
2026-07-18 19:44:29 -0300

    Create RootLayout component with metadata

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Keops",
+  description: "Marketplace Inteligente",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
