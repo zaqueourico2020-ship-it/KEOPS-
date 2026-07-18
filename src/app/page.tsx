92fc0921086d1cbfdc5d4dc8f24a09d19e06c883
zaqueourico2020-ship-it <zaqueourico2020@gmail.com>
2026-07-18 19:46:22 -0300

    Add Home component with main layout

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#0F172A",
+        color: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>KEOPS</h1>
+      <p>Marketplace Inteligente</p>
+    </main>
+  );
+}
